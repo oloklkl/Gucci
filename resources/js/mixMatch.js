89787deb6b9d0739cac0f6067206806d3182e820
oloklkl <sungju7694@naver.com>
2025-05-20 23:38:42 +0900

    mixMatch.js 파일 생성 및 기본 성별 버튼 토글 기능 추가

---
+++ resources/js/mixMatch.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const genderTab = document.querySelector(".gender-tabs");
+  const button = genderTab.querySelector("button");
+  const womanSlides = document.querySelector(".woman-slides");
+  const manSlides = document.querySelector(".man-slides");
+  const addToCartBtn = document.querySelector(".add-to-cart span");
+
+  button.addEventListener("click", () => {
+    const isWoman = button.dataset.gender === "woman";
+
+    if (isWoman) {
+      // 남자로 전환
+      button.dataset.gender = "man";
+      button.innerHTML = `<span>MAN<i class="ri-circle-fill"></i></span>`;
+      button.classList.add("man");
+      addToCartBtn.classList.add("man");
+
+      womanSlides.classList.remove("active");
+      manSlides.classList.add("active");
+    } else {
+      // 여자로 전환
+      button.dataset.gender = "woman";
+      button.innerHTML = `<span><i class="ri-circle-fill"></i>WOMAN</span>`;
+      button.classList.remove("man");
+      addToCartBtn.classList.remove("man");
+
+      womanSlides.classList.add("active");
+      manSlides.classList.remove("active");
+    }
+  });
+});
